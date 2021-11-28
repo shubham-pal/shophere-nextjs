b6eea4a870509b820f9669e0b48a1db45abaacd9
Shubham Pal <shubham.mca19.du@gmail.com>
2021-11-28 17:02:40 +0530

    Nav Footer Layout add

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,41 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>ShopHere</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://app.snipcart.com" />
+        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.css" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="">ShopHere</a>
+          ShopHere
         </h1>
+
+        <p className={styles.description}>
+          <a className="snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none"}}>
+            <strong>Cart:</strong> <span className="snipcart-total-price">₹0.00</span>
+          </a>
+        </p>
 
         <div className={styles.grid}>
           {products.map(product => {
             return (
               <div key={product.id} className={styles.card}>
+                <img src={product.image} alt={`Preview of ${product.title}`} />
                 <h3>{product.title}</h3>
                 <p>{product.description}</p>
                 <p>₹{product.price}</p>
                 <p>
-                  <button>Add to Cart</button>
+                  <button className="snipcart-add-item"
+                    data-item-id={product.id}
+                    data-item-image={product.image}
+                    data-item-name={product.title}
+                    data-item-price={product.price}
+                  >
+                    Add to Cart
+                  </button>
                 </p>
               </div>
             );
@@ -32,13 +49,14 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
+        <script async src="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.js" />
+        <div hidden id="snipcart" data-api-key="NWYzYzc0MjMtOTZlZC00MjE0LWEwMzMtOTc1MDYyZDYyMmU1NjM3NzM2Mzg5MTgwMjAwMzc3" data-config-modal-style="side" />
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          ShopHere
         </a>
       </footer>
     </div>
